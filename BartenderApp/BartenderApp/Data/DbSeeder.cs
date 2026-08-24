48ab11e3ab2727bd099983defe4e246fc12d483c
wait-whatdayisit <gblom073000@gmail.com>
2026-08-24 17:17:20 -0400

    Add DB seeder class

---
+++ BartenderApp/BartenderApp/Data/DbSeeder.cs
@@ -0,0 +1,22 @@
+﻿using BartenderApp.Models;
+
+namespace BartenderApp.Data;
+
+public class DbSeeder
+{
+    public static void Seed(AppDbContext context)
+    {
+        // Only seeds if the table is empty
+        if (context.Drinks.Any()) return;
+
+        // ID, name, description, price, ordered at
+        context.Drinks.AddRange(
+            new Drink { Name = "Widget A", Description = "A great widget", Price = 9.99m },
+            new Drink { Name = "Widget B", Description = "An even better widget", Price = 19.99m },
+            new Drink { Name = "Widget C", Description = "The best widget", Price = 29.99m }
+        );
+
+        context.SaveChanges();
+    }
+
+}
--- BartenderApp/BartenderApp/obj/Debug/net10.0/BartenderApp.AssemblyInfo.cs
+++ BartenderApp/BartenderApp/obj/Debug/net10.0/BartenderApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BartenderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8db6f5a8b5e32251b669fec8e45b22c3ef63d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32bf76b221be28c8bf1912d7366eee16a6a2d8a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BartenderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BartenderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
