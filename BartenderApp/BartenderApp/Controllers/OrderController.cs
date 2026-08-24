9e562c2cd231a60cc57f6301e5641f4769408656
wait-whatdayisit <gblom073000@gmail.com>
2026-08-24 19:00:30 -0400

    Made menu list view

--- BartenderApp/BartenderApp/Controllers/OrderController.cs
+++ BartenderApp/BartenderApp/Controllers/OrderController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BartenderApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BartenderApp.Controllers;
 
 public class OrderController : Controller
 {
+    
+    private readonly AppDbContext _context;
+
+    public OrderController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
     // GET
     public IActionResult Index()
     {
-        return View();
+        var drinks = _context.Drinks.ToList();
+        return View(drinks);
+
     }
 }
--- BartenderApp/BartenderApp/obj/Debug/net10.0/BartenderApp.AssemblyInfo.cs
+++ BartenderApp/BartenderApp/obj/Debug/net10.0/BartenderApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BartenderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2abcecb647cb29a83a2fd2f28c6cf371cb8d2750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406ee1acfc2ea782ecad66b60169af8e17d05a0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BartenderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BartenderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
